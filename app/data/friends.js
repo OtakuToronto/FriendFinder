4ec04af09667e00aab7a28f6722346a7be77021b
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-04-18 17:43:08 -0400

    changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,47 +6,47 @@ let friendsArray = [
     "photo":"https://www.geek.com/wp-content/uploads/2017/08/Dragon-Ball-Super-Goku-625x352.jpg",
     "scores":[
         3,
-        5,
-        5,
-        5,
-        2,
-        1,
+        3,
+        4,
+        3,
+        3,
+        3,
         4,
         2,
         3,
-        4
+        2
       ]
   },
   {
     "name":"Prince Vegeta",
     "photo":"https://pbs.twimg.com/profile_images/710545888750510080/IpRSDNnK_400x400.jpg",
     "scores":[
-        3,
-        2,
-        4,
+        5,
         1,
-        2,
-        2,
-        3,
-        4,
-        3,
-        2
+        5,
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        1
       ]
   },
   {
     "name":"Master Roshi",
     "photo":"https://upload.wikimedia.org/wikipedia/en/e/e0/Kame-Sennin_First_Appearance.jpg",
     "scores":[
-        2,
+        1,
         1,
         2,
         2,
         3,
-        4,
+        5,
         1,
         2,
         2,
-        3
+        4
       ]
   },
   {
@@ -54,30 +54,30 @@ let friendsArray = [
     "photo":"https://upload.wikimedia.org/wikipedia/en/3/37/Android_18.png",
     "scores":[
         3,
-        4,
+        3,
+        3,
         2,
-        1,
         2,
         4,
         3,
         5,
         3,
-        2
+        1
       ]
   },
   {
     "name":"Bulma CC",
     "photo":"https://upload.wikimedia.org/wikipedia/en/9/9a/Bulma.jpg",
     "scores":[
-        2,
-        3,
+        1,
+        5,
         4,
         4,
         5,
         2,
         3,
         2,
-        2,
+        3,
         2
       ]
   },
@@ -86,11 +86,11 @@ let friendsArray = [
     "photo":"https://upload.wikimedia.org/wikipedia/en/4/4b/Piccolo_Dragon_Ball.jpg",
     "scores":[
         3,
-        1,
+        3,
         1,
         5,
         3,
-        1,
+        2,
         1,
         3,
         1,
